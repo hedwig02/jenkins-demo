628399e4d4415d573e316e3a18fc377255e29f19
hedwig02 <sejalsakhala02@gmail.com>
2026-04-20 12:02:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
 function add(a, b) {
   return a + b;
 }
 
-console.log("DevSecOps demo app is running");
+app.get('/', (req, res) => {
+  res.send('DevSecOps App running on AKS 🚀');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
 
 module.exports = { add };
